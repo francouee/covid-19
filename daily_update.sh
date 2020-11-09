e022fc5f0947139aac044a4de905f04288de7834
François <francois.culiere@gmail.com>
2020-11-09 09:37:45 +0100

    updated data

--- daily_update.sh
+++ daily_update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 conda activate covid19
-python /Users/Francois/Documents/Programmation/Python/Divers/Coronavirus/src/generate_plot.py
-current_date="${date +"%d/%m/%y"}"
+python /Users/Francois/Documents/Programmation/Python/Divers/Coronavirus/src/generate_plot_html.py
+current_date=${date +"%d/%m/%y"}
 git add .
 git commit -m "updated data ${current_date}"
 git push
